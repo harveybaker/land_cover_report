4beba1e234f818dc40d6a6b510794bc2958075c5
Harvey Baker <harveymarshbaker@gmail.com>
2023-10-12 12:28:43 +0100

    Creation of land cover jupyter notebooks

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # land_cover_report
 This repo creates a land cover report of a provided kml file using google earth engine.
+
+# Follow steps here to authenticate earth engine
+https://developers.google.com/earth-engine/guides/python_install
---
+++ src/python/earth_engine_run.py
@@ -0,0 +1,142 @@
+land_cover_dict_2021 = {
+    "Tree cover": {
+        "color": "#006400",
+        "value": 10,
+    },
+    "Shrubland": {
+        "color": "#ffbb22",
+        "value": 20,
+    },
+    "Grassland": {
+        "color": "#ffff4c",
+        "value": 30,
+    },
+    "Cropland": {
+        "color": "#f096ff",
+        "value": 40,
+    },
+    "Built-up": {
+        "color": "#fa0000",
+        "value": 50,
+    },
+    "Bare / sparse vegetation": {
+        "color": "#b4b4b4",
+        "value": 60,
+    },
+    "Snow and ice": {
+        "color": "#f0f0f0",
+        "value": 70,
+    },
+    "Permanent water bodies": {
+        "color": "#0064c8",
+        "value": 80,
+    },
+    "Herbaceous wetland": {
+        "color": "#0096a0",
+        "value": 90,
+    },
+    "Mangroves": {
+        "color": "#00cf75",
+        "value": 95,
+    },
+    "Moss and lichen": {
+        "color": "#fae6a0",
+        "value": 100,
+    },
+}
+
+
+
+forrest_2015 = [
+    # 'Unknown. No or not enough satellite data available.',
+    # 'Shrubs. Woody perennial plants with persistent and woody stems\nand without any defined main stem being less than 5 m tall. The\nshrub foliage can be either evergreen or deciduous.\n',
+    # 'Herbaceous vegetation. Plants without persistent stem or shoots above ground\nand lacking definite firm structure. Tree and shrub cover is less\nthan 10 %.\n',
+    # 'Cultivated and managed vegetation / agriculture. Lands covered with temporary crops followed by harvest\nand a bare soil period (e.g., single and multiple cropping systems).\nNote that perennial woody crops will be classified as the appropriate\nforest or shrub land cover type.\n',
+    # 'Urban / built up. Land covered by buildings and other man-made structures.',
+    # 'Bare / sparse vegetation. Lands with exposed soil, sand, or rocks and never has\nmore than 10 % vegetated cover during any time of the year.\n',
+    # 'Snow and ice. Lands under snow or ice cover throughout the year.',
+    # 'Permanent water bodies. Lakes, reservoirs, and rivers. Can be either fresh or salt-water bodies.',
+    'Herbaceous wetland. Lands with a permanent mixture of water and herbaceous\nor woody vegetation. The vegetation can be present in either salt,\nbrackish, or fresh water.\n',
+    'Moss and lichen.',
+    'Closed forest, evergreen needle leaf. Tree canopy >70 %, almost all needle leaf trees remain\ngreen all year. Canopy is never without green foliage.\n',
+    'Closed forest, evergreen broad leaf. Tree canopy >70 %, almost all broadleaf trees remain\ngreen year round. Canopy is never without green foliage.\n',
+    'Closed forest, deciduous needle leaf. Tree canopy >70 %, consists of seasonal needle leaf\ntree communities with an annual cycle of leaf-on and leaf-off\nperiods.\n',
+    'Closed forest, deciduous broad leaf. Tree canopy >70 %, consists of seasonal broadleaf\ntree communities with an annual cycle of leaf-on and leaf-off periods.\n',
+    'Closed forest, mixed.',
+    'Closed forest, not matching any of the other definitions.',
+    'Open forest, evergreen needle leaf. Top layer- trees 15-70 % and second layer- mixed of shrubs\nand grassland, almost all needle leaf trees remain green all year.\nCanopy is never without green foliage.\n',
+    'Open forest, evergreen broad leaf. Top layer- trees 15-70 % and second layer- mixed of shrubs\nand grassland, almost all broadleaf trees remain green year round.\nCanopy is never without green foliage.\n',
+    'Open forest, deciduous needle leaf. Top layer- trees 15-70 % and second layer- mixed of shrubs\nand grassland, consists of seasonal needle leaf tree communities with\nan annual cycle of leaf-on and leaf-off periods.\n',
+    'Open forest, deciduous broad leaf. Top layer- trees 15-70 % and second layer- mixed of shrubs\nand grassland, consists of seasonal broadleaf tree communities with\nan annual cycle of leaf-on and leaf-off periods.\n',
+    'Open forest, mixed.',
+    'Open forest, not matching any of the other definitions.',
+    # 'Oceans, seas. Can be either fresh or salt-water bodies.'
+]
+
+forrest_2019 = [
+    # 'Unknown. No or not enough satellite data available.',
+    # 'Shrubs. Woody perennial plants with persistent and woody stems\nand without any defined main stem being less than 5 m tall. The\nshrub foliage can be either evergreen or deciduous.\n',
+    # 'Herbaceous vegetation. Plants without persistent stem or shoots above ground\nand lacking definite firm structure. Tree and shrub cover is less\nthan 10 %.\n',
+    # 'Cultivated and managed vegetation / agriculture. Lands covered with temporary crops followed by harvest\nand a bare soil period (e.g., single and multiple cropping systems).\nNote that perennial woody crops will be classified as the appropriate\nforest or shrub land cover type.\n',
+    # 'Urban / built up. Land covered by buildings and other man-made structures.',
+    # 'Bare / sparse vegetation. Lands with exposed soil, sand, or rocks and never has\nmore than 10 % vegetated cover during any time of the year.\n',
+    # 'Snow and ice. Lands under snow or ice cover throughout the year.',
+    # 'Permanent water bodies. Lakes, reservoirs, and rivers. Can be either fresh or salt-water bodies.',
+    'Herbaceous wetland. Lands with a permanent mixture of water and herbaceous\nor woody vegetation. The vegetation can be present in either salt,\nbrackish, or fresh water.\n',
+    'Moss and lichen.',
+    'Closed forest, evergreen needle leaf. Tree canopy >70 %, almost all needle leaf trees remain\ngreen all year. Canopy is never without green foliage.\n',
+    'Closed forest, evergreen broad leaf. Tree canopy >70 %, almost all broadleaf trees remain\ngreen year round. Canopy is never without green foliage.\n',
+    'Closed forest, deciduous needle leaf. Tree canopy >70 %, consists of seasonal needle leaf\ntree communities with an annual cycle of leaf-on and leaf-off\nperiods.\n',
+    'Closed forest, deciduous broad leaf. Tree canopy >70 %, consists of seasonal broadleaf\ntree communities with an annual cycle of leaf-on and leaf-off periods.\n',
+    'Closed forest, mixed.',
+    'Closed forest, not matching any of the other definitions.',
+    'Open forest, evergreen needle leaf. Top layer- trees 15-70 % and second layer- mixed of shrubs\nand grassland, almost all needle leaf trees remain green all year.\nCanopy is never without green foliage.\n',
+    'Open forest, evergreen broad leaf. Top layer- trees 15-70 % and second layer- mixed of shrubs\nand grassland, almost all broadleaf trees remain green year round.\nCanopy is never without green foliage.\n',
+    'Open forest, deciduous needle leaf. Top layer- trees 15-70 % and second layer- mixed of shrubs\nand grassland, consists of seasonal needle leaf tree communities with\nan annual cycle of leaf-on and leaf-off periods.\n',
+    'Open forest, deciduous broad leaf. Top layer- trees 15-70 % and second layer- mixed of shrubs\nand grassland, consists of seasonal broadleaf tree communities with\nan annual cycle of leaf-on and leaf-off periods.\n',
+    'Open forest, mixed.',
+    'Open forest, not matching any of the other definitions.',
+    # 'Oceans, seas. Can be either fresh or salt-water bodies.'
+]
+
+forrest_2021 = [
+    'Tree cover',
+    'Shrubland',
+    'Grassland',
+    # 'Cropland',
+    # 'Built-up',
+    # 'Bare / sparse vegetation',
+    # 'Snow and ice',
+    # 'Permanent water bodies',
+    'Herbaceous wetland',
+    'Mangroves',
+    'Moss and lichen'
+]
+
+forrest_2009
+
+globcover_2009_dict 
+Value	Color	Description
+11	#aaefef	Post-flooding or irrigated croplands
+14	#ffff63	Rainfed croplands
+20	#dcef63	Mosaic cropland (50-70%) / vegetation (grassland, shrubland, forest) (20-50%)
+30	#cdcd64	Mosaic vegetation (grassland, shrubland, forest) (50-70%) / cropland (20-50%)
+40	#006300	Closed to open (>15%) broadleaved evergreen and/or semi-deciduous forest (>5m)
+50	#009f00	Closed (>40%) broadleaved deciduous forest (>5m)
+60	#aac700	Open (15-40%) broadleaved deciduous forest (>5m)
+70	#003b00	Closed (>40%) needleleaved evergreen forest (>5m)
+90	#286300	Open (15-40%) needleleaved deciduous or evergreen forest (>5m)
+100	#788300	Closed to open (>15%) mixed broadleaved and needleleaved forest (>5m)
+110	#8d9f00	Mosaic forest-shrubland (50-70%) / grassland (20-50%)
+120	#bd9500	Mosaic grassland (50-70%) / forest-shrubland (20-50%)
+130	#956300	Closed to open (>15%) shrubland (<5m)
+140	#ffb431	Closed to open (>15%) grassland
+150	#ffebae	Sparse (>15%) vegetation (woody vegetation, shrubs, grassland)
+160	#00785a	Closed (>40%) broadleaved forest regularly flooded - Fresh water
+170	#009578	Closed (>40%) broadleaved semi-deciduous and/or evergreen forest regularly flooded - saline water
+180	#00dc83	Closed to open (>15%) vegetation (grassland, shrubland, woody vegetation) on regularly flooded or waterlogged soil - fresh, brackish or saline water
+190	#c31300	Artificial surfaces and associated areas (urban areas >50%) GLOBCOVER 2009
+200	#fff5d6	Bare areas
+210	#0046c7	Water bodies
+220	#ffffff	Permanent snow and ice
+230	#743411	Unclassified
